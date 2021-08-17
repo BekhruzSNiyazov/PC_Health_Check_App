5a4cb7598e1b95073e343ac7aed3220b6ffb2ed6
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-08-17 16:54:42 +0500

    better UI/UX

--- src/front-end/static/scripts/helpers.js
+++ src/front-end/static/scripts/helpers.js
@@ -13,7 +13,7 @@ const generate = (text, percent) => {
 
     const used = document.createElement("div");
     used.style.width = percent + "%";
-    used.style.backgroundColor = percent > 70 ? "#eb4646" : "#15e626";
+    used.style.backgroundColor = percent > 75 ? "#eb4646" : percent > 50 ? "#ebeb26" : "#15e626";
     used.style.borderRadius = "10px";
 
     const free = document.createElement("p");
@@ -32,7 +32,7 @@ const separate = () => addHTML("<br style='clear: both;'>");
 const update = async (elements, percent) => {
     elements[0].element.innerText = elements[0].element.innerText.split("(")[0] + "(" + percent + "%)";
     elements[1].style.width = percent + "%";
-    elements[1].style.backgroundColor = percent > 70 ? "#eb4646" : "#15e626";
+    elements[1].style.backgroundColor = percent > 75 ? "#eb4646" : percent > 50 ? "#ebeb26" : "#15e626";
     elements[2].style.width = 100 - percent + "%";
     elements[3].style.backgroundColor = getCookie("app-theme") === "dark" ? "gray" : "lightgray";
 }
